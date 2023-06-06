5d74e1910b0d843a7cf75cd1a688c8823795904a
Topik-Hub <108158864+eeeerrrrf3@users.noreply.github.com>
2023-06-06 15:00:29 +0300

    Update script.js

--- case/script.js
+++ case/script.js
@@ -1,11 +1,1 @@
-// Загрузка сохраненных данных из куков
-function loadSavedData() {
-    var money = getCookie("money");
-    if (money) {
-        money = parseInt(money);
-        updateMoney(money);
-    }
-    
-    var items = getCookie("items");
-    if (items) {
-        items
+s
